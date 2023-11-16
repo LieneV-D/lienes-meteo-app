efe645864b9e91f854a29a9a3f66a3f341e7603b
LieneV-D <vecvagare.liene@gmail.com>
2023-11-16 18:19:59 +0200

    Added search functionality using API

--- src/app.js
+++ src/app.js
@@ -1,11 +1,26 @@
+function updateWeather(response) {
+  let temperatureElement = document.querySelector("#current-stats-value");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  let CityElementOne = document.querySelector("#city-search-one");
+  CityElementOne.innerHTML = response.data.city;
+  let CityElementTwo = document.querySelector("#city-search-two");
+  CityElementTwo.innerHTML = response.data.city;
+}
+
+function searchCity(city) {
+  let apiKey = "23e75f7acb80e43d1aa43c2eod7017bt";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
 function changeCity(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#city-search-input");
-  let h1Element = document.querySelector("#city-search-one");
-  let h2Element = document.querySelector("#city-searh-two");
-  h1Element.innerHTML = searchInput.value;
-  h2Element.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let citySearchElement = document.querySelector("#city-search-form");
 citySearchElement.addEventListener("submit", changeCity);
+
+searchCity("Riga");
